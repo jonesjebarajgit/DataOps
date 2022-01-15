6d5b6ef07c42a2d5df699b0558ac6c28f059ff77
jonesjebarajgit <78785585+jonesjebarajgit@users.noreply.github.com>
2022-01-16 01:16:23 +0530

    reverting some changes

--- e2e_samples/parking_sensors_synapse/deploy.sh
+++ e2e_samples/parking_sensors_synapse/deploy.sh
@@ -34,7 +34,6 @@ export SYNAPSE_SQL_PASSWORD='Jones@001'
 export PROJECT
 export ENV_NAME
 export GITHUB_REPO_URL='https://github.com/jonesjebarajgit/DataOps'
-export SYNAPSE_WORKSPACE_NAME='synapsews'
 
 . ./scripts/common.sh
 . ./scripts/verify_prerequisites.sh
